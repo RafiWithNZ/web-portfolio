f8baa6635b9fee70895c86df627d9c7e9fe2f948
RafiWithNZ <rafinuril100@gmail.com>
2024-11-21 13:03:12 +0700

    add all

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import moneytoring from '../assets/portfolio/moneytoring.png'
+import griyapets from '../assets/portfolio/griyapets.png'
+import bpbd from '../assets/portfolio/bpbd.png'
+
+const Portfolio = () => {
+
+    const cards = [
+        {
+            id: 1,
+            title: 'Aplikasi Pencatatan Transaksi e-wallet atau mobile banking otomatis melalui Notifikasi',
+            src: moneytoring,
+            href: 'https://github.com/xcapri/MoneytoringApp'
+        },
+        {
+            id: 2,
+            title: 'Project UI/UX Aplikasi Petcare "Griyapets"',
+            src: griyapets,
+            href: 'https://docs.google.com/presentation/d/1wyZvNQUA0ORuYrhdXEFgLK5lKdwL-CWAPysi--hYMXo/edit#slide=id.g174caa8877a_0_52'
+        },
+        {
+            id: 3,
+            title: 'Website BPBD Kota Surabaya',
+            src: bpbd,
+            href: 'https://github.com/ArloDel/bpbd_surabaya'
+        },
+    ]
+
+    return (
+        <div Name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen' >
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-gray-400'>Portfolio</p>
+                    <p className='py-6'>Explore my technical skills: A showcase of my front-end web development projects.</p>
+                </div>
+
+
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+                    {
+                        cards.map(({ id, src, title, href }) => (
+                            <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                                <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
+                                <div className='flex items-center justify-center'>
+                                    <a href={href} target="_blank" rel="noopener noreferrer">
+                                        <button className=' px-6 py-3 m-4 duration-200 hover:scale-125'>{title}</button>
+                                    </a>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio
